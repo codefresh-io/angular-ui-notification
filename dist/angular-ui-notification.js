9fcb35da52b530b0ae9456b46686d1cdd1a5b61c
benmeiri <ben@codefresh.io>
2017-02-10 13:10:42 -0800

    fixed minified

--- dist/angular-ui-notification.js
+++ dist/angular-ui-notification.js
@@ -70,7 +70,7 @@ angular.module('ui-notification').provider('Notification', function() {
                 // load it via $http only if it isn't default template and template isn't exist in template cache
                 // cache:true means cache it for later access.
                 $http.get(args.template,{cache: true})
-                   .then(function(response){
+                  .then(function(response){
                     processNotificationTemplate(response.data);
                   })
                   .catch(function(data){
